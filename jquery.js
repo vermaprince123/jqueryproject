f3993952099f32256814ea3dd85ce0dd94e2c70e
prince verma <prince.verma@dgliger.com>
2021-10-22 11:20:24 +0530

    first commit

---
+++ jquery.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("button.bulbon").click(function(){
+        $("img").attr('src','pic_bulbof.gif');
+    })
+
+    $("button.bulbof").click(function(){
+        $("img").attr('src','pic_bulbon.gif');
+    })
+})
